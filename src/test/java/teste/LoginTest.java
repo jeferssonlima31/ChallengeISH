987288c7215844f8ad84ffde55914af794841fea
Jefersson Lima <jeferssonlima4@hotmail.com>
2024-11-27 12:22:04 -0300

    melhorando o codigo

--- src/test/java/teste/LoginTest.java
+++ src/test/java/teste/LoginTest.java
@@ -7,39 +7,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.Login;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import static helpers.Constants.*;
 
 public class LoginTest {
 
-    static WebDriver driver;
-    static Login login;
-
+    private static WebDriver driver;
+    private static Login login;
 
     @BeforeAll
-    public static void setUp() throws Exception{
-        System.setProperty("webdriver.chrome.driver",basicPathChrome);
+    public static void setUp() {
+        // Configura o driver do Chrome
+        System.setProperty("webdriver.chrome.driver", basicPathChrome);
         driver = new ChromeDriver();
-        driver.get(loginURL);
-        login = new Login(driver);
+
+        // Configurações do navegador
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+        // Navega até a URL de login
+        driver.get(loginURL);
 
+        // Inicializa a página de login
+        login = new Login(driver);
     }
 
     @Test
-    public void loginTest(){
-
+    public void testLogin() {
+        // Realiza o preenchimento do formulário de login
         login.fillOutForm();
     }
 
     @AfterAll
-   public  static void closeAfterToExecute() throws Exception{
-
-        Thread.sleep(3000);
-        driver.quit();
-
+    public static void tearDown() {
+        if (driver != null) {
+            
+            driver.quit();
+        }
     }
 }
